aaee9b4b42694de7ef21c1bc25c6998062e8c671
Kevin <kevincespedes2004@gmail.com>
2022-03-02 10:25:42 -0500

    new improvements

--- app.js
+++ app.js
@@ -11,11 +11,51 @@ async function getPokemon(url) {
     let nexLink;
     let template = "";
 
-    console.log(json);
+    if (!res.ok) throw { status: res.status, statusText: res.statusText };
+
+    for (let i = 0; i < json.results.length; i++) {
+      try {
+        let url = json.results[i].url;
+        let res = await fetch(url);
+        let pokemon = await res.json();
+
+        if (!res.ok) throw { status: res.status, statusText: res.statusText };
+
+        const gif =
+          pokemon["sprites"]["versions"]["generation-v"]["black-white"][
+            "animated"
+          ]["front_default"];
+        const imgPokemon = pokemon.sprites.front_default;
+
+        template += `
+        <div class="pokemon">
+          <img src=${imgPokemon} alt =${pokemon.name}">
+          <h2>${pokemon.name}</h2>
+        </div>`;
+      } catch (err) {
+        let message = err.statusText || "Ocurrió un error, intente nuevamente";
+        template = `
+          <p>Error: ${err.status} ${message}</p>
+        `;
+      }
+    }
+    main.innerHTML = template;
+    prevLink = json.previous ? `<a href="${json.previous}">⏪</a>` : "";
+    nexLink = json.next ? `<a href="${json.next}">⏭️</a>` : "";
+
+    links.innerHTML = prevLink + "" + nexLink;
   } catch (err) {
     let message = err.statusText || "Ocurrió un error, intente nuevamente";
-    main.innerHTML = `<p> Error: ${message}</p>`;
+    main.innerHTML = `<p> Error: ${err.status} ${message}</p>`;
   }
 }
 
-getPokemon(pokeApi);
+document.addEventListener("DOMContentLoaded", (e) => {
+  getPokemon(pokeApi);
+});
+document.addEventListener("click", (e) => {
+  if (e.target.matches(".links a")) {
+    e.preventDefault();
+    getPokemon(e.target.href);
+  }
+});
